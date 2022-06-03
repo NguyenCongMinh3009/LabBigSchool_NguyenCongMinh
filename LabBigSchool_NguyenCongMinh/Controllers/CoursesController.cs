eb0ba4ff934dccd616015d2aac8c6d28ffbf6cb9
congminh3009@gmail.com <congminh3009@gmail.com>
2022-06-03 10:50:09 +0700

    Add project files.

---
+++ LabBigSchool_NguyenCongMinh/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LabBigSchool_NguyenCongMinh.Models;
+using LabBigSchool_NguyenCongMinh.ViewModels;
+
+namespace LabBigSchool_NguyenCongMinh.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ LabBigSchool_NguyenCongMinh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabBigSchool_NguyenCongMinh.Startup))]
+namespace LabBigSchool_NguyenCongMinh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
